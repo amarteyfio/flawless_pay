f0e3797dc296b9c2ba0aefa04be0d1f670193261
amarteyfio <amarteyfio>
2022-11-21 01:35:29 +0000

    cred

--- Settings/db_cred.php
+++ Settings/db_cred.php
@@ -1,8 +1,16 @@
 <?php
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
 //credentials
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'flawless_pay');
+define('DB_SERVER', $cleardb_url["host"]);
+define('DB_USERNAME', $cleardb_url["user"]);
+define('DB_PASSWORD', $cleardb_url["pass"]);
+define('DB_NAME', substr($cleardb_url["path"],1));
+
+
+$active_group = 'default';
+$query_builder = TRUE;
+
 
 ?>
